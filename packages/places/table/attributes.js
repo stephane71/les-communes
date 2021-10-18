c9e7d266cd8f3fb828e57e61901b1148f45fbdc1
stephane71 <stephanemaire71@gmail.com>
2021-10-18 09:45:33 +0200

    Places | table : add DynamoDB table definition variables

---
+++ packages/places/table/attributes.js
@@ -0,0 +1,6 @@
+export const SLUG = "slug";
+export const COUNTY = "county";
+export const TYPE = "type";
+export const GEOHASH = "geohash";
+export const CODE = "code";
+export const POPULATION = "population";
---
+++ packages/places/table/globalSecondaryIndexes.js
@@ -0,0 +1,4 @@
+export const CountyTypeGSI = "CountyTypeGSI";
+export const GeohashTypeGSI = "GeohashTypeGSI";
+export const CodeTypeGSI = "CodeTypeGSI";
+export const TypePopulationGSI = "TypePopulationGSI";
