17df6f323501718fc480c4d148ab84fba3f49160
stephane71 <stephanemaire71@gmail.com>
2021-12-15 19:48:41 +0100

    Webapp | AppBar : create IconButtonDarkMode that handle dark mode toggle

--- packages/webapp/components/AppBar/index.js
+++ packages/webapp/components/AppBar/index.js
@@ -1,7 +1,32 @@
+import { useContext } from "react";
+import { useTheme } from "@mui/material/styles";
 import Link from "next/link";
 import AppBarMui from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import LinkMui from "@mui/material/Link";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import ColorModeContext from "../../contexts/ColorModeContext";
+
+const IconButtonDarkMode = () => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <IconButton
+      sx={{ ml: 1 }}
+      onClick={colorMode.toggleColorMode}
+      color="inherit"
+    >
+      {theme.palette.mode === "dark" ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
+  );
+};
 
 function AppBar() {
   return (
@@ -23,6 +48,7 @@ function AppBar() {
             Les Communes
           </LinkMui>
         </Link>
+        <IconButtonDarkMode />
       </Toolbar>
     </AppBarMui>
   );
