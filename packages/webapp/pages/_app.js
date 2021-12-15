d00a42ba0dcbb48a90e3d6c416bdcfbe2188ea06
stephane71 <stephanemaire71@gmail.com>
2021-12-15 19:47:44 +0100

    Webapp | app : add ColorModeContext && handle setMode

--- packages/webapp/pages/_app.js
+++ packages/webapp/pages/_app.js
@@ -1,12 +1,13 @@
 import * as React from "react";
+import { useMemo, useState } from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
-import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import AppBar from "../components/AppBar";
+import ColorModeContext from "../contexts/ColorModeContext";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -20,6 +21,26 @@ const SEO = {
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -27,13 +48,15 @@ export default function MyApp(props) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="description" content={SEO.description} />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar />
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </ThemeProvider>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppBar />
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </CacheProvider>
   );
 }
