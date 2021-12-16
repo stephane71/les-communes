38b8f6b219da2bc3eac8fdf79373f2d1b74a3961
stephane71 <stephanemaire71@gmail.com>
2021-12-16 13:57:14 +0100

    Webapp | app : use SEO from enums

--- packages/webapp/pages/_app.js
+++ packages/webapp/pages/_app.js
@@ -8,15 +8,10 @@ import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "../src/createEmotionCache";
 import AppBar from "../components/AppBar";
 import ColorModeContext from "../contexts/ColorModeContext";
+import { SEO } from "../src/enums";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
-
-const SEO = {
-  title: "Les Communes | Retrouvez les informations sur votre commune",
-  description:
-    "Les Communes met à disposition les informations indispensables de votre communes : population, coordonnées, code postal",
-};
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
@@ -44,9 +39,9 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{SEO.title}</title>
+        <title>{SEO.HOME.title}</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta name="description" content={SEO.description} />
+        <meta name="description" content={SEO.HOME.description} />
       </Head>
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
