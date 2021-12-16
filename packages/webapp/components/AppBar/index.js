e7b58579e295178da3d540e131129c244eb39e39
stephane71 <stephanemaire71@gmail.com>
2021-12-16 14:06:40 +0100

    Webapp | AppBar : add Container maw width "md"

--- packages/webapp/components/AppBar/index.js
+++ packages/webapp/components/AppBar/index.js
@@ -5,6 +5,7 @@ import AppBarMui from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import LinkMui from "@mui/material/Link";
 import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import ColorModeContext from "../../contexts/ColorModeContext";
@@ -45,19 +46,21 @@ function AppBar() {
         color: theme.palette.text.secondary,
       }}
     >
-      <Toolbar>
-        <Link href="/" passHref>
-          <LinkMui
-            underline="none"
-            color={theme.palette.text.secondary}
-            variant="h6"
-            sx={{ flexGrow: 1 }}
-          >
-            {APP_NAME}
-          </LinkMui>
-        </Link>
-        <IconButtonDarkMode />
-      </Toolbar>
+      <Container maxWidth="md">
+        <Toolbar>
+          <Link href="/" passHref>
+            <LinkMui
+              underline="none"
+              color={theme.palette.text.secondary}
+              variant="h6"
+              sx={{ flexGrow: 1 }}
+            >
+              {APP_NAME}
+            </LinkMui>
+          </Link>
+          <IconButtonDarkMode />
+        </Toolbar>
+      </Container>
     </AppBarMui>
   );
 }
