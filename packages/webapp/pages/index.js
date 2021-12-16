297d2d31f9afe80f703f8fe26f31d0e11b27b973
stephane71 <stephanemaire71@gmail.com>
2021-12-16 13:58:18 +0100

    Webapp | index : improve styles

--- packages/webapp/pages/index.js
+++ packages/webapp/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { useRouter } from "next/router";
+import { useTheme } from "@mui/material/styles";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Link from "@mui/material/Link";
+import { APP_NAME } from "../src/enums";
 
 const exemples = [
   { title: "Lyon", county: "rhone", slug: "lyon" },
@@ -15,8 +17,22 @@ const exemples = [
   { title: "Charolles", county: "saone-et-loire", slug: "charolles" },
 ];
 
+const AppNameSpan = () => {
+  const theme = useTheme();
+  return (
+    <Typography
+      component="span"
+      color={theme.palette.text.secondary}
+      fontWeight={theme.typography.fontWeightBold}
+    >
+      {APP_NAME}
+    </Typography>
+  );
+};
+
 export default function Index() {
   const router = useRouter();
+  const theme = useTheme();
 
   function handleClickCityItem(county, slug) {
     return () => {
@@ -26,11 +42,22 @@ export default function Index() {
 
   return (
     <Container maxWidth="sm" sx={{ pt: 4 }}>
-      <Typography variant="h3" component="h1" gutterBottom>
+      <Typography
+        variant="h3"
+        component="h1"
+        gutterBottom
+        fontWeight={theme.typography.fontWeightBold}
+        color={theme.palette.info.dark}
+      >
         Les informations essentielles de votre commune
       </Typography>
-      <Typography variant="p" gutterBottom>
-        Les Communes met à disposition une page dédiée pour chaque commune
+
+      <Typography
+        variant="p"
+        gutterBottom
+        color={theme.palette.text.secondary}
+      >
+        <AppNameSpan /> met à disposition une page dédiée pour chaque commune
         française. Retrouvez le code postal, la population et la carte de votre
         commune.
       </Typography>
