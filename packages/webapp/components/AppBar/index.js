22e425e252e28e0aaf686d8e6a76a07a053419bc
stephane71 <stephanemaire71@gmail.com>
2021-12-16 13:58:35 +0100

    Webapp | AppBar : improve styles

--- packages/webapp/components/AppBar/index.js
+++ packages/webapp/components/AppBar/index.js
@@ -8,6 +8,7 @@ import IconButton from "@mui/material/IconButton";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import ColorModeContext from "../../contexts/ColorModeContext";
+import { APP_NAME } from "../../src/enums";
 
 const IconButtonDarkMode = () => {
   const theme = useTheme();
@@ -29,23 +30,30 @@ const IconButtonDarkMode = () => {
 };
 
 function AppBar() {
+  const theme = useTheme();
+
   return (
     <AppBarMui
       position="static"
       color="transparent"
       variant="outlined"
       elevation={0}
-      sx={{ border: 0, borderBottom: 1, borderColor: "grey.300" }}
+      sx={{
+        border: 0,
+        borderBottom: 1,
+        borderColor: "grey.300",
+        color: theme.palette.text.secondary,
+      }}
     >
       <Toolbar>
         <Link href="/" passHref>
           <LinkMui
             underline="none"
-            color="text.primary"
+            color={theme.palette.text.secondary}
             variant="h6"
             sx={{ flexGrow: 1 }}
           >
-            Les Communes
+            {APP_NAME}
           </LinkMui>
         </Link>
         <IconButtonDarkMode />
