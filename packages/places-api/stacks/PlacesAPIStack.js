77a25844bfcc15e4361df13a21025e522084403d
stephane71 <stephanemaire71@gmail.com>
2022-01-19 09:44:56 +0100

    Places API | PlacesAPIStack : use ApiGatewayV1Api && add usage plan

--- packages/places-api/stacks/PlacesAPIStack.js
+++ packages/places-api/stacks/PlacesAPIStack.js
@@ -1,14 +1,16 @@
 import * as sst from "@serverless-stack/resources";
 import Table, { TABLE_NAME } from "../src/table";
 import Api, { API_NAME } from "../src/api";
+import usagePlan from "../src/usagePlan";
 
 export default class PlacesAPIStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
     const table = new sst.Table(this, TABLE_NAME, Table);
-    const api = new sst.Api(this, API_NAME, Api);
+    const api = new sst.ApiGatewayV1Api(this, API_NAME, Api);
 
+    api.restApi.addUsagePlan(usagePlan.id, usagePlan.usagePlanProps);
     api.attachPermissions([table]);
 
     // Show the endpoint in the output
