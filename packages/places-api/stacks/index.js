3ae96fe22a5f89ea2f9ec03a12c8100ceabf152b
stephane71 <stephanemaire71@gmail.com>
2022-01-19 09:43:29 +0100

    Places API | stacks : update stack name

--- packages/places-api/stacks/index.js
+++ packages/places-api/stacks/index.js
@@ -2,10 +2,8 @@ import PlacesAPIStack from "./PlacesAPIStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
+    runtime: "nodejs14.x",
   });
 
-  new PlacesAPIStack(app, "places-api-stack");
-
-  // Add more stacks
+  new PlacesAPIStack(app, "crud-stack");
 }
