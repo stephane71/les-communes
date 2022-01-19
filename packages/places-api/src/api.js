7e040d0e189937b95077ca1efb449a6c22f7d77d
stephane71 <stephanemaire71@gmail.com>
2022-01-19 09:44:15 +0100

    Places API | api : remove PUT && add apiKeyRequired on GET

--- packages/places-api/src/api.js
+++ packages/places-api/src/api.js
@@ -1,16 +1,20 @@
-import { ApiAuthorizationType } from "@serverless-stack/resources";
 import { TABLE_NAME } from "./table";
 
 export const API_NAME = "PlacesApi";
 
 const API = {
-  defaultAuthorizationType: ApiAuthorizationType.AWS_IAM,
   defaultFunctionProps: {
     environment: { TABLE_NAME },
   },
   routes: {
-    "GET /{county}/{slug}": "src/get/lambda.handler",
-    "PUT /{county}/{slug}": "src/put/lambda.handler",
+    "GET /{county}/{slug}": {
+      function: {
+        handler: "src/get/lambda.handler",
+      },
+      methodOptions: {
+        apiKeyRequired: true,
+      },
+    },
   },
 };
 
