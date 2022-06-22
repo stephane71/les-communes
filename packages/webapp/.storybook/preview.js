0e7ddb81decb3e1041b1d428ca83928185c4248b
stephane71 <stephanemaire71@gmail.com>
2022-06-22 12:30:13 +0200

    Webapp | storybook preview.js: wrap stories with ThemeProvider && CssBaseline

--- packages/webapp/.storybook/preview.js
+++ packages/webapp/.storybook/preview.js
@@ -1,3 +1,17 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../src/theme";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +20,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
